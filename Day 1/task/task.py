63d801921fc0fe15fef331de2632f5eb6cb69bf4
Ben Joseph <bjoseph@sourceallies.com>
2024-10-31 13:32:04 -0500

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Band Name Generator")
+city = input("What city did you grow up in?\n")
+pet = input("What is your pet's name?\n")
+
+print("Your new band name could be, " + city + " " + pet)
---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,4 @@
+print("Number of letters in your name: " + len(input("Enter your name"))
+
+name = len(input("Enter your name"))
+print(name)
