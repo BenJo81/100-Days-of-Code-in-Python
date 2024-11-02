d75e14746754f7259549be2742c6a7b638895230
Ben Joseph <bjoseph@sourceallies.com>
2024-11-02 12:13:38 -0500

    Day 3

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,8 @@
+number = int(input("What number shall I check? "))
+
+if number % 2 == 0:
+    print("This is an even number")
+    age = int(input("What is your age? "))
+    if age <=
+else:
+    print("This is an odd number")
