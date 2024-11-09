8062bfd20d38b6f5b37566402fd276fe7bdb1711
Ben Joseph <ben.joseph81@gmail.com>
2024-11-09 16:45:45 -0600

    Day 9

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -2,5 +2,31 @@
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
 # TODO-4: Compare bids in dictionary
+from tarfile import TruncatedHeaderError
 
+import art
+print(art.logo)
+bids = {}
+prog_cont = True
 
+print("Welcome to the secret auction program.")
+
+while prog_cont:
+    name = input("What is your name? ")
+    bid = int(input("What is your bid? $"))
+
+    bids[name] = bid
+
+    yes_or_no = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if yes_or_no == "no":
+        prog_cont = False
+        high_bid = 0
+        winner = ""
+        for key in bids:
+            if (key[0]) > high_bid:
+                high_bid = bids[key]
+                winner = key
+        print(f"The winner of the auction is {winner} and they bid an amount of ${high_bid}")
+
+    else:
+        if yes_or_no == "yes":
--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -1,1 +1,20 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again.",
+    "Loop": "The action of doing something over and over again.",
+}
+
+# print(programming_dictionary["Bug"])
+#
+# programming_dictionary["Loop"] = "The action of doing something over and over again"
+# print(programming_dictionary)
+#
+# empty_list = []
+# empty_dictionary = {}
+#
+# programming_dictionary["Bug"] = "A moth in your computer"
+# print(programming_dictionary)
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
