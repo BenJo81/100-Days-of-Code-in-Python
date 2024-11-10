89ff3d9a2a445867b0f04ccc4c354b8251bd5f48
Ben Joseph <ben.joseph81@gmail.com>
2024-11-10 10:24:55 -0600

    Day 10

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,47 @@
+from art import logo
+
 def add(n1, n2):
     return n1 + n2
 
 
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+def calculator():
+    print(logo)
+    run = True
+
+    number1 = float(input("What is the first number? "))
+
+    while run:
+        operation = input("+\n-\n*\n/\nPick an operation: ")
+        number2 = float(input("What is the next number? "))
+
+        result = operations[operation](number1, number2)
+
+        print(f"{number1} {operation} {number2} = {result}")
+        go_again = input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+        if go_again == "y":
+            number1 = result
+        else:
+            run = False
+            print("\n" * 20)
+            calculator()
+
+calculator()
---
+++ Day 10/Functions with Outputs/task.py
@@ -0,0 +1,8 @@
+def format_name(f_name, l_name):
+    x = f_name.title()
+    y = l_name.title()
+    return f"{x} {y}"
+
+formated_string = format_name("BEN", "JOSEPH")
+
+print(formated_string)
