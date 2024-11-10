db899c7dd683ca00e544d69bdeaba18279b68a36
Ben Joseph <ben.joseph81@gmail.com>
2024-11-10 08:36:39 -0600

    Day 9b

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -15,18 +15,16 @@ while prog_cont:
     name = input("What is your name? ")
     bid = int(input("What is your bid? $"))
 
-    bids[name] = bid
+    bids.update({name: bid})
 
     yes_or_no = input("Are there any other bidders? Type 'yes' or 'no'.\n")
     if yes_or_no == "no":
         prog_cont = False
         high_bid = 0
         winner = ""
-        for key in bids:
-            if (key[0]) > high_bid:
-                high_bid = bids[key]
+        for key, value in bids.items():
+            if value > high_bid:
+                high_bid = value
                 winner = key
         print(f"The winner of the auction is {winner} and they bid an amount of ${high_bid}")
 
-    else:
-        if yes_or_no == "yes":
